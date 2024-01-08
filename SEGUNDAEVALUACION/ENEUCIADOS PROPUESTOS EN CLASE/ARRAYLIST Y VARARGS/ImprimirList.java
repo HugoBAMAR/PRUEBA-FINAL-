959bb0c67dd2be5a4219b0c535b61e5f87d86d56
HugoBAMAR <149675587+HugoBAMAR@users.noreply.github.com>
2024-01-08 20:35:18 +0100

    Apuntes y ejercicios de la segunda evaluacion

--- SEGUNDAEVALUACION/ENEUCIADOS PROPUESTOS EN CLASE/ARRAYLIST Y VARARGS/ImprimirList.java
+++ SEGUNDAEVALUACION/ENEUCIADOS PROPUESTOS EN CLASE/ARRAYLIST Y VARARGS/ImprimirList.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
 public class ImprimirList {
     public static void main (String[] args) {
-        static Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Por favor ingrese el numero de  argumentos que desea para alamcenar en una lista");
-        int cantArgs = sc.nextInt;
+        int cantArgs = sc.nextInt();
+        sc.nextLine();
 
-        String[] args = new String[cantArgs];
+        String[] argus = new String[cantArgs];
 
         for (int i = 0; i < cantArgs; i++) {
             System.out.println("Por favor ingrese una palabra");
-            args[i] = sc.nextLine();
+            argus[i] = sc.nextLine();
+
         }
-        imprimirArgs(args);
+        imprimirArgs(argus);
     }
     public static void imprimirArgs (String... args){
-        for (String argumentos: args)
-            System.out.println(args);
+        for (String argumentos: args) {
+            System.out.println(argumentos);
+        }
     }
 }
